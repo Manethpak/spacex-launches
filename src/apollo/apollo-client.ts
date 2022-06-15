795cf4922c18b293b46450c075a4feb485580806
Maneth Pak <manethpak00@gmail.com>
2022-06-15 16:49:15 +0700

    add query and modify codegen

--- src/apollo/apollo-client.ts
+++ src/apollo/apollo-client.ts
@@ -1,8 +1,17 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { offsetLimitPagination } from "@apollo/client/utilities";
 
 const client = new ApolloClient({
   uri: "https://api.spacex.land/graphql/",
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          launchesPast: offsetLimitPagination(),
+        },
+      },
+    },
+  }),
 });
 
 export default client;
