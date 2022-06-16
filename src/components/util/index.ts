df19438ccf594bec00a168e515a40944fe49ee9f
Maneth Pak <manethpak00@gmail.com>
2022-06-16 23:41:33 +0700

    modualize

---
+++ src/components/util/index.ts
@@ -0,0 +1,4 @@
+export { default as GithubSVG } from "./GithubSVG";
+export { default as LoadingSpinner } from "./LoadingSpinner";
+export { default as ScrollToTop } from "./ScrollToTop";
+export { default as SuccessBadge } from "./SuccessBadge";
