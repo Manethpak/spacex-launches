a4bcbdaeca6d29488a09d8515a6c4a26b7803953
Maneth Pak <manethpak00@gmail.com>
2022-06-16 23:44:35 +0700

    modulize

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as LaunchCard } from "./LaunchCard";
+export { default as DetailError } from "./DetailError";
+export { default as DetailLoading } from "./DetailLoading";
+export { default as DetailLaunchInfo } from "./DetailLaunchInfo";
+export { default as DetailRocketInfo } from "./DetailRocketInfo";
